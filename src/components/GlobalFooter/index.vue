c7f098f9c3c2f7fd0028bbe0c1dda74525676ca4
Jaylin Lee <apx103@foxmail.com>
2022-11-25 22:30:58 +0800

    [init] Start Dev

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -2,7 +2,7 @@
   <global-footer class="footer custom-render">
     <template v-slot:links>
       <a href="http://ci.pjlab.org.cn" target="_blank">CI 平台</a>
-      <a href="http://dashborad.pjlab.org.cn" target="_blank">Dashboard</a>
+      <a href="http://dashboard.pjlab.org.cn" target="_blank">Dashboard</a>
       <a href="https://www.github.com/APX103/" target="_blank">@APX103</a>
     </template>
   </global-footer>
