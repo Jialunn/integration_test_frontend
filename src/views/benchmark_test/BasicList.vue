35379cbc644157cd3c96897826eb11878e85cb75
Jaylin Lee <apx103@foxmail.com>
2023-02-22 16:53:43 +0800

    [dev]

--- src/views/benchmark_test/BasicList.vue
+++ src/views/benchmark_test/BasicList.vue
@@ -85,47 +85,6 @@ data.push({
     value: 90
   }
 })
-data.push({
-  title: 'Angular',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png',
-  description: '希望是一个好东西，也许是最好的，好东西是不会消亡的',
-  owner: '曲丽丽',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 54
-  }
-})
-data.push({
-  title: 'Ant Design',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png',
-  description: '生命就像一盒巧克力，结果往往出人意料',
-  owner: '林东东',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 66
-  }
-})
-data.push({
-  title: 'Ant Design Pro',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png',
-  description: '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
-  owner: '周星星',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 30
-  }
-})
-data.push({
-  title: 'Bootstrap',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png',
-  description: '那时候我只会想自己想要什么，从不想自己拥有什么',
-  owner: '吴加好',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    status: 'exception',
-    value: 100
-  }
-})
 
 export default {
   name: 'StandardList',
--- src/views/integration_test/BasicList.vue
+++ src/views/integration_test/BasicList.vue
@@ -1,6 +1,6 @@
 <template>
   <page-header-wrapper>
-    <a-card :bordered="false">
+    <!-- <a-card :bordered="false">
       <a-row>
         <a-col :sm="8" :xs="24">
           <info title="我的待办" value="8个任务" :bordered="true" />
@@ -12,7 +12,13 @@
           <info title="本周完成任务数" value="24个" />
         </a-col>
       </a-row>
-    </a-card>
+    </a-card> -->
+
+    <div v-for="(title, index) in barTitle" :key="index">
+      <a-card style="margin-top: 24px" :bordered="false">
+        <bar :data="metrics[index]" :title="title" />
+      </a-card>
+    </div>
 
     <a-card
       style="margin-top: 24px"
@@ -73,6 +79,7 @@
 // 演示如何使用 this.$dialog 封装 modal 组件
 import TaskForm from './modules/TaskForm'
 import Info from './components/Info'
+import { Bar } from '@/components'
 
 const data = []
 data.push({
@@ -85,59 +92,229 @@ data.push({
     value: 90
   }
 })
-data.push({
-  title: 'Angular',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png',
-  description: '希望是一个好东西，也许是最好的，好东西是不会消亡的',
-  owner: '曲丽丽',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 54
-  }
-})
-data.push({
-  title: 'Ant Design',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png',
-  description: '生命就像一盒巧克力，结果往往出人意料',
-  owner: '林东东',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 66
-  }
-})
-data.push({
-  title: 'Ant Design Pro',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png',
-  description: '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
-  owner: '周星星',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    value: 30
-  }
-})
-data.push({
-  title: 'Bootstrap',
-  avatar: 'https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png',
-  description: '那时候我只会想自己想要什么，从不想自己拥有什么',
-  owner: '吴加好',
-  startAt: '2018-07-26 22:44',
-  progress: {
-    status: 'exception',
-    value: 100
-  }
-})
+
+const resData = {
+  'code': 0,
+  'data': [
+    {
+      '_id': '63e202518fac92b83686a7e8',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230207',
+      'test_started_time': 1675756113,
+      'success': false,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3790'
+        }
+      }
+    },
+    {
+      '_id': '63e21702e0b38d66629aa683',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_test_1715',
+      'test_started_time': 1675761410,
+      'success': false,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'err': 'can not get metric'
+        }
+      }
+    },
+    {
+      '_id': '63e337c7977545d62018937f',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230208',
+      'test_started_time': 1675835335,
+      'success': true,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.4340'
+        }
+      }
+    },
+    {
+      '_id': '63e34a08d340d8966a498444',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230207_2',
+      'test_started_time': 1675840008,
+      'success': false,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3860'
+        }
+      }
+    },
+    {
+      '_id': '63e371b44537899acedb8811',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchamrk_20230208_2',
+      'test_started_time': 1675850164,
+      'success': false,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'err': 'can not get metric'
+        }
+      }
+    },
+    {
+      '_id': '63e38a89071d384979040458',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_demo',
+      'test_started_time': 1675856521,
+      'success': true,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3950'
+        }
+      }
+    },
+    {
+      '_id': '63e492927cedda98f5d6022e',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230209',
+      'test_started_time': 1675924114,
+      'success': true,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3950'
+        }
+      }
+    },
+    {
+      '_id': '63e49e0786699336f1ec1d43',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230209_2',
+      'test_started_time': 1675927047,
+      'success': false,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3790'
+        },
+        'baseline': {
+          'bbox_mAP': 39.4
+        }
+      }
+    },
+    {
+      '_id': '63e663e8609a9f867339174e',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_test_20230210',
+      'test_started_time': 1676043240,
+      'success': true,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3950'
+        },
+        'baseline': {
+          'bbox_mAP': 39.4
+        }
+      }
+    },
+    {
+      '_id': '63e9ab106e837956897e2adc',
+      'repo': 'mmdetection',
+      'branch': 'dev-3.x',
+      'case': 'atss_r50_fpn_1x_coco',
+      'config': 'configs/atss/atss_r50_fpn_1x_coco.py',
+      'test_version': 'benchmark_20230213',
+      'test_started_time': 1676258064,
+      'success': true,
+      'results': {
+        'dataset': 'coco',
+        'eval': [
+          'bbox'
+        ],
+        'metrics': {
+          'coco/bbox_mAP': '0.3950'
+        },
+        'baseline': {
+          'bbox_mAP': 39.4
+        }
+      }
+    }
+  ]
+}
 
 export default {
   name: 'StandardList',
   components: {
     TaskForm,
-    Info
+    Info,
+    Bar
   },
   data () {
     return {
       data,
+      barTitle: [],
+      chartData: [],
       status: 'all'
     }
+  },
+  created () {
+    this.loadChartData()
   },
   methods: {
     add () {
@@ -190,6 +367,35 @@ export default {
           centered: true,
           maskClosable: false
         })
+    },
+    loadChartData () {
+      let head = ''
+      // eslint-disable-next-line
+      let title = []
+      // eslint-disable-next-line
+      let metrics = []
+      const d = resData.data
+      for (const i in d) {
+        if (head === '') {
+          head = d[i].case
+        }
+        Object.keys(d[i].results.metrics).forEach((key) => {
+          if (key !== 'err') {
+            const name = head + ' : ' + key
+            if (title.indexOf(name) === -1) {
+              title.push(name)
+              metrics.push([])
+            }
+            const index = title.indexOf(name)
+            metrics[index].push({
+              x: d[i].branch + ';' + d[i].test_version,
+              y: d[i].results.metrics[key]
+            })
+          }
+        })
+      }
+      this.barTitle = title
+      this.metrics = metrics
     }
   }
 }
